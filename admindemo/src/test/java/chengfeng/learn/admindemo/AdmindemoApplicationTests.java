53b58db8fb85a42501ffc6b63952466a1647d0b4
chengfengzhai <chengfeng.zhai@gmail.com>
2022-01-31 19:28:04 -0500

    init project with files generated from start.spring.io, only changed the .gitignore to let git ignore maven jars files included in original zip file

---
+++ admindemo/src/test/java/chengfeng/learn/admindemo/AdmindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package chengfeng.learn.admindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmindemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
